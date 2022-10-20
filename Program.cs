6456ea85d4dae640d4e56a7c4806ed6d56b50c79
Brand0nLe <93brandonle@gmail.com>
2022-10-20 08:57:22 -0700

    Final commit

--- Program.cs
+++ Program.cs
@@ -2,9 +2,7 @@
 //10-20-22
 //Mini Challenge 5 - Mad Lib
 //Project will save 10 user inputs as strings and print them out into a mad lib story. 
-//The user has the option to play again or end.
-
-//need data validation
+//The user has the option to play again or end. Also has data validation for both words and numbers in the user inputs and play again.
 
 
 Console.Clear();
